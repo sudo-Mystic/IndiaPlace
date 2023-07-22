1e36297d56d09983f63a21666a16db49781b0c43
Rishabh Bohra <93476337+sudo-Mystic@users.noreply.github.com>
2023-07-22 19:19:22 +0530

    Update placenlbot.user.js

--- placenlbot.user.js
+++ placenlbot.user.js
@@ -7,7 +7,7 @@
 // @match        https://www.reddit.com/r/place/*
 // @match        https://new.reddit.com/r/place/*
 // @connect      reddit.com
-// @connect      commando.burgmoment.repl.co
+// @connect      indiaplace-commandserver.onrender.com
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=reddit.com
 // @require	     https://cdn.jsdelivr.net/npm/toastify-js
 // @resource     TOASTIFY_CSS https://cdn.jsdelivr.net/npm/toastify-js/src/toastify.min.css
